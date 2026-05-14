2c4f1a114dc2cbbc33fcc7ee5f5179121a6fa5c4
bkablam11 <bkablam11@gamil.com>
2026-05-14 17:52:51 +0000

    car: ajout de la version web

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ const List<Map<String, String>> membersList = [
   {
     'name': 'Maguid',
     'role': 'controller',
-    'email': 'maguidouattara@gmail.com',
+    'email': 'bkablam20@gmail.com',
     'init': 'MA',
   },
   {
